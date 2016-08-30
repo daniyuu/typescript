3acc2f7629c921eee91e8807c1f66e380a25c89f
Yueying Chen <yueych@microsoft.com>
2016-08-30 15:41:47 +0800

    restart

--- src/js/Stores/SearchStore.js
+++
@@ -1,4 +0,0 @@
-var AppDispatcher = require('../Dispatchers/AppDispatcher');
-var EventEmitter = require('events').EventEmitter;
-var UCMConstants = require('../Constants/UCMConstants');
-var assign = require('object-assign');
